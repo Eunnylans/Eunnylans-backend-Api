d9f5b89c87e00a20217e779a1ace6373116a2b82
Eunice Abieyuwa Igbinedion <eunice.igbinedion@yahoo.com>
2023-04-17 22:22:40 +0200

    Updating Server File

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const express = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 const app = express();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
